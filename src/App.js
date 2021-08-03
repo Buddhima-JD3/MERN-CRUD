f3503e1422a203e15a5fa57cf4340274ce8a4b50
Buddhima <it20071998@my.sliit.lk>
2021-08-03 23:47:19 +0530

    #comment Crud Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component } from 'react'
+  import {BrowserRouter,Route} from 'react-router-dom'
+  import CreatePost from './components/CreatePost';
+  import EditPost from './components/EditPost';
+  import PostDetails from './components/PostDetails';
+  import Home from './Components/Home';
+  import NavBar from './Components/NavBar';
+  
+  
+  export default class App extends Component {
+    render() {
+      return (
+        <BrowserRouter>
+        <div className='container'>
+          <NavBar/>
+          <Route path="/" exact Component={Home}></Route>
+          <Route path="/add" Component={CreatePost}></Route>
+          <Route path="/edit/:id" Component={EditPost}></Route>
+          <Route path="post/:id" Component={PostDetails}></Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+          
+        </div>
+        </BrowserRouter>
+      )
+    }
+  }
+  
