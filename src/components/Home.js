a06d66b082af90fcc4401fd50704c1ec6ff12aae
Buddhima <it20071998@my.sliit.lk>
2021-08-07 13:43:57 +0530

    #comment edit,create, search done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,72 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 export default class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      posts:[]
+    this.state = {
+      posts: [],
     };
   }
 
-componentDidMount(){
-  this.retrievePosts();
-}
+  componentDidMount() {
+    this.retrievePosts();
+  }
 
-  retrievePosts(){
-    axios.get("/posts").then(res =>{
-      if(res.data.success){
+  retrievePosts() {
+    axios.get("/posts").then((res) => {
+      if (res.data.success) {
         this.setState({
-          posts:res.data.existingPosts
+          posts: res.data.existingPosts,
         });
-        console.log(this.state.posts)
+        console.log(this.state.posts);
       }
     });
   }
+
+  onDelete = (id) => {
+    axios.delete(`/post/delete/${id}`).then((res) => {
+      alert("Deleted Succeefully");
+      this.retrievePosts();
+    });
+  };
+
+  filterData(posts,searchKey){
+    const result = posts.filter((post) =>
+    post.topic.toLowerCase().includes(searchKey)||
+    post.description.toLowerCase().includes(searchKey)||
+    post.postCategory.toLowerCase().includes(searchKey)
+    )
+    this.setState({posts:result})
+  }
+
+  handleSearchArea = (e) => {
+    const searchKey = e.currentTarget.value;
+    axios.get("/posts").then((res) => {
+      if (res.data.success) {
+        this.filterData(res.data.existingPosts, searchKey)
+      }
+    });
+  };
+
   render() {
     return (
-      <div className = "container">
-        <p>All Posts</p>
-        <table className ="table">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-9 mt-2 mb-2">
+            <h4>All Posts</h4>
+          </div>
+          <div className="col-lg-3 mt-2 mb-2">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              name="searchQuery"
+              onChange={this.handleSearchArea}
+            ></input>
+          </div>
+        </div>
+
+        <table className="table table-hover" style={{ marginTop: "40px" }}>
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -38,44 +78,42 @@ componentDidMount(){
           </thead>
 
           <tbody>
-              {this.state.posts.map((posts,index) =>(
-                <tr key={index}>
-                  <th scope = "row">{index +1}</th>
-                  <td>
+            {this.state.posts.map((posts, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>
                   <a
                     href={`/post/${posts._id}`}
                     style={{ textDecoration: "none" }}
                   >
                     {posts.topic}
-                  </a>                            
-                  </td>
-                  <td>{posts.description}</td>
-                  <td>{posts.PostCategory}</td>
-                  <td>{posts.topic}</td>
-                  <td>
-                    <a className = "btn btn-warning" href="#">
-                      <i className = "fas fa-edit"></i>&nbsp;Edit
-                    </a>
-                    &nbsp;
-                    <a className = "btn btn-danger" href="#">
-                      <i className = "fas fa-trash-alt"></i>&nbsp;Delete
-                    </a>
-
-                  </td>
-
-                </tr>
-              ))}
-
-
+                  </a>
+                </td>
+                <td>{posts.description}</td>
+                <td>{posts.topic}</td>
+                <td>
+                  <a className="btn btn-warning" href={`/edit/${posts._id}`}>
+                    <i className="fas fa-edit"></i>&nbsp;Edit
+                  </a>
+                  &nbsp;
+                  <a
+                    className="btn btn-danger"
+                    href="#"
+                    onClick={() => this.onDelete(posts._id)}
+                  >
+                    <i className="fas fa-trash-alt"></i>&nbsp;Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
-
-
-
-
-
         </table>
-        <button className="btn btn-success"><a href="/add" style={{textDecoration:'none',color:'white'}}>Create New Post</a></button>
+        <button className="btn btn-success">
+          <a href="/add" style={{ textDecoration: "none", color: "white" }}>
+            Create New Post
+          </a>
+        </button>
       </div>
-    )
+    );
   }
 }
